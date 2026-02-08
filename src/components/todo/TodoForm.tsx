99a2bc470a01469ff08869721b353240af0755c3
LeeJaein <devxoox1106@gmail.com>
2026-02-08 18:04:34 +0900

    feat: TodoForm 컴포넌트 구현

---
+++ src/components/todo/TodoForm.tsx
@@ -0,0 +1,33 @@
+import TextField from '@/components/common/TextField';
+import Button from '@/components/common/Button';
+import PlusIcon from '@/components/icons/PlusIcon';
+
+export interface TodoFormProps {
+  inputValue: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onSubmit: (e: React.SyntheticEvent) => void;
+}
+
+const TodoForm = ({ inputValue, onChange, onSubmit }: TodoFormProps) => {
+  return (
+    <div className="w-full">
+      <form className="flex gap-10" onSubmit={onSubmit}>
+        <TextField
+          placeholder="할 일을 입력하세요"
+          name="name"
+          variant="search"
+          onChange={onChange}
+        />
+        <Button
+          variant={inputValue.trim() ? 'addActive' : 'addDefault'}
+          size="large"
+          icon={<PlusIcon />}
+        >
+          추가하기
+        </Button>
+      </form>
+    </div>
+  );
+};
+
+export default TodoForm;
